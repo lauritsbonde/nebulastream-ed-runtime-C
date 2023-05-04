7e7bad384a5bb9f5b4f7eda8bae4d06b876251d4
Markus Johansen <markusraunjohansen@gmail.com>
2023-05-04 21:50:34 +0200

    run expressions on board

--- src/main.c
+++ src/main.c
@@ -7,12 +7,11 @@
 #include "stack.h"
 #include "environment.h"
 #include "expression.h"
-// #include "./proto/EndDeviceProtocol.pb.h"
+#include "EndDeviceProtocol.pb.h"
 #include "operators.h"
 #include "number.h"
 #include "encodeInput.h"
-// #include "./tests/runTest.h"
-// #include "./tests/testType.h"
+#include "ztimer.h"
 
 // Macros
 // #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(*arr))
@@ -52,21 +51,26 @@ int main(void)
 
     printf("size: %d\n", e->size);
 
-    // // Construct example expression to be evaluated (5+5)
-    // Expression exp;
-    // Instruction i1 = {{CONST}, 0};
-    // Instruction i2 = {.data._int=5, 2};
-    // Instruction i3 = {{CONST}, 0};
-    // Instruction i4 = {.data._int=5, 2};
-    // Instruction i5 = {{ADD}, 0};
-    // Instruction program[5] = {i1, i2, i3, i4, i5};
-    // exp.p_size = 5;
-    // exp.program = program;
+    // Construct example expression to be evaluated (5+5)
+    Expression exp;
+    Instruction i1 = {{CONST}, 0};
+    Instruction i2 = {.data._int=5, 2};
+    Instruction i3 = {{CONST}, 0};
+    Instruction i4 = {.data._int=5, 2};
+    Instruction i5 = {{ADD}, 0};
+    Instruction program[5] = {i1, i2, i3, i4, i5};
+    exp.p_size = 5;
+    exp.program = program;
+    exp.env = e;
+    exp.stack = e->stack;
 
-    // Number res = call(&exp);
-    // printf("%d\n",res.type._int);
-    // printf("%d\n",exp.p_size);
+    Number res = call(&exp);
+    printf("5 + 5 = %d\n",res.type._int);
+    
+    ztimer_sleep(ZTIMER_SEC, 5);
   }
+
+  
   
   return 0;
 }
